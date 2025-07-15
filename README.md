171320acd7aa1aee5262f227288c02490e89f78d
TimMcWalter <timmcwalter@gmail.com>
2025-07-15 23:28:18 +0200

    end of day

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NewNostra
 new version of nostra
+
+# FAKER
+Creates sample data set for later analysis in the bounderies given
---
+++ faking.py
@@ -0,0 +1,16 @@
+from faking import Faker
+
+
+
+
+fake = Faker()
+
+
+class Fake_set():
+    def __init__(self, name):
+        self.name = name
+
+
+    def create():
+        
+        pass
